39baab94230eed1a0b3125199f6bc329472c2a1a
Arshi Bhasin <1354333+middleverse@users.noreply.github.com>
2022-08-02 13:44:21 -0400

    update init.sh for style

--- init.sh
+++ init.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 echo "Initializing William."
+
 # TASK: add william as a cronjob
 # write out current crontab
 crontab -l > mycron
+
 # copy script to /usr/local/bin
 cp william.sh /usr/local/bin/william.sh
+
 # insert new cron job
 echo "01 22 * * * bash /usr/local/bin/william.sh" >> mycron
+
 # install new cron file and get rid of the temp cron
 crontab mycron && rm mycron
+
 echo "William is at your service."
