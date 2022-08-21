aa990a721268753228ff9da86cadc3dffc778f7b
Arshi Bhasin <1354333+middleverse@users.noreply.github.com>
2022-08-21 17:48:37 -0400

    william cron script attempt #1

--- william.sh
+++ william.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
+YEARMONTH=$(date +"%Y-%m")
+SECOND=$(date +"%s")
 
-touch $HOME/bin/william_temp.txt
+cd $HOME/bin/mackenzie
 
+git pull
+
+# create log file if it doesn't exit
+if [[ ! -f "${YEARMONTH}.txt" ]]
+then 
+    touch ${YEARMONTH}.txt && echo "> today's log file created"
+fi
+
+# add to file
+echo "Another line ${SECOND}" >> ${YEARMONTH}.txt
+
+git add -A
+git commit -m "yes sarge"
+git push
