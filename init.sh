add4b4be62de08c6034bfedf3d7d179a633d681e
Arshi Bhasin <arshi_b@hotmail.com>
2022-10-02 15:13:22 -0400

    create .env file and export variables during initialization script

--- init.sh
+++ init.sh
@@ -17,14 +17,24 @@ sudo crontab -u $USER -l > mycron
 cp william.sh $WILLIAMPATH/william.sh
 chmod a+x $WILLIAMPATH/william.sh && echo "> copied script and made executable"
 
+# create a logfile
+touch $WILLIAMPATH/william.log
+
 # insert new cron job
 echo "01 22 * * * cd $WILLIAMPATH && ./william.sh" >> mycron
 
 # install new cron file and get rid of the temp cron
 sudo crontab -u $USER mycron && rm mycron && echo "> added cron to $USER's crontab"
 
-echo "> William is at your service."
-
 echo "> Setting up mackenzie"
 
 [ ! -d "$USERHOME/bin/mackenzie" ] && git clone git@github.com:middleverse/mackenzie.git $HOME/bin/mackenzie && echo "> mackenzie repo is set up"
+
+echo "> Setting up environment"
+
+echo "export WATSON=$(which watson)" >> $WILLIAMPATH/.env
+echo "export LOGFILE=$WILLIAMPATH/william.log" >> $WILLIAMPATH/.env
+echo "export MACKENZIE=$HOME/bin/mackenzie" >> $WILLIAMPATH/.env
+
+echo "> William is at your service."
+
